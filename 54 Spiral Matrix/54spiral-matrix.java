85b1b7f1b80513ce575e6140db591389cba0a8a8
Hanumithaa <hanumithaagad@gmail.com>
2026-01-07 05:01:23 +0000

    Code challenge submission for question: 54 Spiral Matrix

---
+++ 54 Spiral Matrix/54spiral-matrix.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> li = new ArrayList<>();
+        int left = 0;
+        int right = matrix[0].length-1;
+        int top = 0;
+        int bot=matrix.length-1;
+
+        while(top<=bot && left<=right){
+            for(int i=left; i<=right;i++){
+                li.add(matrix[top][i]);
+            }
+            top++;
+
+            for(int i=top; i<=bot;i++){
+                li.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bot) {
+               for(int i=right; i>=left;i--){
+                li.add(matrix[bot][i]);
+            }
+            bot--;
+            }
+
+            if (left <= right) { 
+            for(int i=bot; i>=top;i--){
+                li.add(matrix[i][left]);
+            }
+            left++;
+            }
+
+            
+        }
+        return li;
+
+
+        
+    }
+}
