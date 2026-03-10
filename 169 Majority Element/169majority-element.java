a38c1746295be2f94fb818f201242f143b5b7e25
Hanumithaa <hanumithaagad@gmail.com>
2026-03-10 05:11:57 +0000

    Code challenge submission for question: 169 Majority Element

---
+++ 169 Majority Element/169majority-element.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        int ele=0;
+
+        for(int i=0; i<n; i++){
+            if(count==0){
+                ele=nums[i];
+            }
+            
+            if(nums[i]==ele){
+                count++;
+            }
+            else{
+                count--;
+            }
+            
+            
+        }
+        return ele;
+
+    
+        
+    }
+}
