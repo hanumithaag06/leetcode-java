f427f86dbea28053c534dff72de964eea08fe234
Hanumithaa <hanumithaagad@gmail.com>
2026-04-08 07:20:58 +0000

    Code challenge submission for question: 1823 Find the Winner of the Circular Game

---
+++ 1823 Find the Winner of the Circular Game/1823find-the-winner-of-the-circular-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        return joseph(n,k)+1;
+        
+        
+    }
+    public int joseph(int n, int k){
+        
+        if(n==1){
+            return 0;
+        }
+        return (joseph(n-1,k) + k) % n;
+    }
+}
