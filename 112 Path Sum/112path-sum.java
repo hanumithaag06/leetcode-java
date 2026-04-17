f44fa8ea244a8e53aad6e7045c533af8f68a9b15
Hanumithaa <hanumithaagad@gmail.com>
2026-04-17 05:30:37 +0000

    Code challenge submission for question: 112 Path Sum

---
+++ 112 Path Sum/112path-sum.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null){
+            return false;
+        }
+
+        if(root.left==null && root.right==null){
+            if(root.val==targetSum){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        int rsum=targetSum-root.val;
+        boolean lcheck=hasPathSum(root.left,rsum);
+        boolean rcheck=hasPathSum(root.right,rsum);
+        return lcheck || rcheck;
+
+        
+    }
+}
