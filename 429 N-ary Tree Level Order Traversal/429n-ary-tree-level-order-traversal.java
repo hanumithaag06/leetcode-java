d718d6e23c16b9919b20cc86a8b720d82e0e755b
Hanumithaa <hanumithaagad@gmail.com>
2026-07-01 05:45:34 +0000

    Code challenge submission for question: 429 N-ary Tree Level Order Traversal

---
+++ 429 N-ary Tree Level Order Traversal/429n-ary-tree-level-order-traversal.java
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> l=new ArrayList<>();
+        if (root==null) return l;
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> curlist = new ArrayList<>();
+            for(int i=0; i<size; i++){
+                Node curr = q.poll();
+                curlist.add(curr.val); 
+                for(Node child : curr.children){
+                    q.offer(child);
+                }                         
+
+
+            }
+            l.add(curlist);
+        }
+
+        return l;
+        
+    }
+}
